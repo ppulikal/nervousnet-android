9e374d0fda32ea0e6714e6518cc451adb047a6ba
Prasad <prasad7779@gmail.com>
2016-04-12 18:18:31 +0200

    Added new Noisemaker extension project

---
+++ MobileClients/Android/SampleExtensions/Noisemeter/src/ch/ethz/coss/nervousnet/extensions/noisemeter/AboutActivity.java
@@ -0,0 +1,16 @@
+package ch.ethz.coss.nervousnet.extensions.noisemeter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_about);
+
+	}
+
+}
