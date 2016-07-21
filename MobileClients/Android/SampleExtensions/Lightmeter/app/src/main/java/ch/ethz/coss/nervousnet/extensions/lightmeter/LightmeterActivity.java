738aea765053309a3170feedd58f95362f71d549
Prasad <prasad7779@gmail.com>
2016-07-21 17:21:19 +0200

    updated lightemeter sample extension with library version 0.7.18 features

--- MobileClients/Android/SampleExtensions/Lightmeter/app/src/main/java/ch/ethz/coss/nervousnet/extensions/lightmeter/LightmeterActivity.java
+++ MobileClients/Android/SampleExtensions/Lightmeter/app/src/main/java/ch/ethz/coss/nervousnet/extensions/lightmeter/LightmeterActivity.java
@@ -26,6 +26,7 @@ import ch.ethz.coss.nervousnet.lib.ErrorReading;
 import ch.ethz.coss.nervousnet.lib.LibConstants;
 import ch.ethz.coss.nervousnet.lib.LightReading;
 import ch.ethz.coss.nervousnet.lib.NervousnetRemote;
+import ch.ethz.coss.nervousnet.lib.NervousnetSensorDataListener;
 import ch.ethz.coss.nervousnet.lib.NervousnetServiceConnectionListener;
 import ch.ethz.coss.nervousnet.lib.NervousnetServiceController;
 import ch.ethz.coss.nervousnet.lib.RemoteCallback;
@@ -33,7 +34,7 @@ import ch.ethz.coss.nervousnet.lib.SensorReading;
 import ch.ethz.coss.nervousnet.lib.Utils;
 
 
-public class LightmeterActivity extends Activity implements NervousnetServiceConnectionListener {
+public class LightmeterActivity extends Activity implements NervousnetServiceConnectionListener, NervousnetSensorDataListener {
 
 	int m_interval = 100; // 100 milliseconds by default, can be changed later
 	Handler m_handler = new Handler();
@@ -42,6 +43,8 @@ public class LightmeterActivity extends Activity implements NervousnetServiceCon
 	NervousnetServiceController nervousnetServiceController;
 	TextView lux, errorView;
 	LinearLayout reading, error;
+
+	SensorReading lReading;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -93,109 +96,41 @@ public class LightmeterActivity extends Activity implements NervousnetServiceCon
 	}
 
 
-	protected void update(NervousnetRemote mService) throws RemoteException{
-		if (mService != null) {
+
+	protected void update() throws RemoteException{
 			Log.d("LightmeterActivity", "before updating 1");
-			SensorReading lReading = mService.getLatestReading(LibConstants.SENSOR_LIGHT);
+			SensorReading lReading = nervousnetServiceController.getLatestReading(LibConstants.SENSOR_LIGHT);
 			if(lReading != null) {
-					if(lReading instanceof LightReading)
-						Log.d("LightmeterActivity", "before updating 3");
-					lux.setText("" + ((LightReading) lReading).getLuxValue());
-					reading.setVisibility(View.VISIBLE);
-					error.setVisibility(View.INVISIBLE);
+					if(lReading instanceof LightReading) {
+						Log.d("LightmeterActivity", "LightReading found");
+						lux.setText("" + ((LightReading) lReading).getLuxValue());
+						reading.setVisibility(View.VISIBLE);
+						error.setVisibility(View.INVISIBLE);
+					} else if(lReading instanceof ErrorReading) {
+						Log.d("LightmeterActivity", "ErrorReading found");
+						lux.setText("Error Code: " + ((ErrorReading) lReading).getErrorCode() + ", " + ((ErrorReading) lReading).getErrorString());
+						reading.setVisibility(View.VISIBLE);
+						error.setVisibility(View.INVISIBLE);
+
+					}
 				}else  {
 					lux.setText("Light object is null");
 					reading.setVisibility(View.INVISIBLE);
 					error.setVisibility(View.VISIBLE);
 				}
-		}else {
-			Toast.makeText(getApplicationContext(), "mService null", Toast.LENGTH_SHORT)
-					.show();
-			 error.setVisibility(View.VISIBLE);
-			 reading.setVisibility(View.INVISIBLE);
-		}
 
 
 	}
 
-//	protected void update() throws RemoteException {
-//
-//		if (mService != null) {
-//			Log.d("LightmeterActivity", "before updating 1");
-////			SensorReading lReading = null;
-////			try {
-//				Log.d("LightmeterActivity", "before updating 2 ");
-//				 final RemoteCallback.Stub cbBinder = new RemoteCallback.Stub() {
-//
-//					@Override
-//					public void success(List list) throws RemoteException {
-//						SensorReading lReading = null;
-//						if(list.size() > 0)
-//							lReading = (SensorReading) list.get(0);
-//						if(lReading != null) {
-//							if(lReading instanceof LightReading)
-//								Log.d("LightmeterActivity", "before updating 3");
-//							lux.setText("" + ((LightReading) lReading).getLuxValue());
-//							reading.setVisibility(View.VISIBLE);
-//							error.setVisibility(View.INVISIBLE);
-//						}else  {
-//							Log.d("LightmeterActivity", "before updating 4 "+list.size());
-//							lux.setText("Light object is null");
-//							reading.setVisibility(View.INVISIBLE);
-//							error.setVisibility(View.VISIBLE);
-//						}
-//					}
-//
-//					@Override
-//					public void failure(ErrorReading reading) throws RemoteException {
-//
-//					}
-//				};
-//
-////			lReading = (SensorReading)
-//					mService.getReading(LibConstants.SENSOR_ACCELEROMETER, cbBinder);
-//
-//
-////				lReading =
-////				SensorReading lReading = null;
-////				Log.d("LightmeterActivity", "before updating 5 "+cbBinder);
-////				lReading =	(SensorReading) mService.getLatestReading(LibConstants.SENSOR_LIGHT);
-////				if(lReading != null) {
-////					if(lReading instanceof LightReading)
-////						Log.d("LightmeterActivity", "before updating 3");
-////					lux.setText("" + ((LightReading) lReading).getLuxValue());
-////					reading.setVisibility(View.VISIBLE);
-////					error.setVisibility(View.INVISIBLE);
-////				}else  {
-////					lux.setText("Light object is null");
-////					reading.setVisibility(View.INVISIBLE);
-////					error.setVisibility(View.VISIBLE);
-////				}
-////				mService.getReading(LibConstants.SENSOR_LIGHT,cbBinder);
-//
-////			} catch (DeadObjectException doe) {
-////				// TODO Auto-generated catch block
-////				doe.printStackTrace();
-////			}
-//		} else {
-//			Toast.makeText(getApplicationContext(), "mService null", Toast.LENGTH_SHORT)
-//					.show();
-//			 error.setVisibility(View.VISIBLE);
-//			 reading.setVisibility(View.INVISIBLE);
-//		}
-//
-//	}
-
-
-	void startRepeatingTask(final NervousnetRemote mService) {
+	void startRepeatingTask() {
 		m_statusChecker = new Runnable() {
 			@Override
 			public void run() {
 
-				Log.d("NervousnetServiceUtil", "before updating");
+				Log.d("LightmeterActivity", "before updating");
 
 				try {
-					update(mService);
+					update();
 				} catch (RemoteException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -213,8 +148,9 @@ public class LightmeterActivity extends Activity implements NervousnetServiceCon
 	}
 
 	@Override
-	public void onServiceConnected(NervousnetRemote mService) {
-		startRepeatingTask(mService);
+	public void onServiceConnected() {
+		startRepeatingTask();
+
 	}
 
 	@Override
@@ -222,7 +158,10 @@ public class LightmeterActivity extends Activity implements NervousnetServiceCon
 		stopRepeatingTask();
 	}
 
-
+	@Override
+	public void onSensorDataReady(SensorReading reading) {
+	this.lReading = reading;
+	}
 
 
 
