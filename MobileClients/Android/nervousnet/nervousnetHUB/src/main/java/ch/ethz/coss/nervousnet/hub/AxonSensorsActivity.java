c766586dfa073705c3f2b3c22905a3900b0ef4a2
birene <birene@student.ethz.ch>
2016-08-05 00:47:47 +0200

    Added new activity for Axon Sensors Permissions. New Layout for Axon Sensors on/off List

---
+++ MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/AxonSensorsActivity.java
@@ -0,0 +1,20 @@
+package ch.ethz.coss.nervousnet.hub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import ch.ethz.coss.nervousnet.hub.ui.BaseActivity;
+
+public class AxonSensorsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_axon_sensors);
+
+        String axonName = getIntent().getStringExtra("AxonName");
+        Toast.makeText(AxonSensorsActivity.this, axonName, Toast.LENGTH_SHORT).show();
+
+    }
+}
--- MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/SensorPermissionsActivity.java
+++ MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/SensorPermissionsActivity.java
@@ -1,26 +1,39 @@
 package ch.ethz.coss.nervousnet.hub;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import ch.ethz.coss.nervousnet.hub.ui.BaseActivity;
 import ch.ethz.coss.nervousnet.hub.ui.adapters.AxonsArrayAdapter;
 import ch.ethz.coss.nervousnet.hub.ui.adapters.NervousnetAxon;
 
-public class SensorPermissionsActivity extends Activity {
+public class SensorPermissionsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_permissions);
 
-        ListView listAxons = (ListView) findViewById(R.id.lst_axonsPermissions);
-        NervousnetAxon[] axonNames = getAxonList();
-        AxonsArrayAdapter adapter = new AxonsArrayAdapter(this, R.layout.axon_list_item, axonNames);
-        listAxons.setAdapter(adapter);
+        ListView listViewAxons = (ListView) findViewById(R.id.lst_axonsPermissions);
+        final NervousnetAxon[] axonArray = getAxonList();
+        AxonsArrayAdapter adapter = new AxonsArrayAdapter(this, R.layout.axon_list_item, axonArray);
+        listViewAxons.setAdapter(adapter);
+        listViewAxons.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(SensorPermissionsActivity.this, AxonSensorsActivity.class);
+                intent.putExtra("AxonName",axonArray[position].axonName);
 
+                startNextActivity(intent);
+            }
+        });
     }
 
     private NervousnetAxon[] getAxonList() {
